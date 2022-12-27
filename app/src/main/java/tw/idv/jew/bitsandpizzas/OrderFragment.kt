efb6c2bc0331c64542d17a1ec0c7c18b9185f5e8
a0916105 <whdofjew@gmail.com>
2022-12-27 09:35:45 +0800

    取得fragment的activity參考來呼叫setSupportActionBar()方法

--- app/src/main/java/tw/idv/jew/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/tw/idv/jew/bitsandpizzas/OrderFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class OrderFragment : Fragment() {
 
@@ -13,6 +15,11 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        val view = inflater.inflate(R.layout.fragment_order, container, false)
+
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        return view
     }
 }
