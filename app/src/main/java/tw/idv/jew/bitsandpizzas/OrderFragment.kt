8d5038bff707bf57b9a71606070dd7c54fddcebf
a0916105 <whdofjew@gmail.com>
2022-12-27 19:56:40 +0800

    Toast和Snackbar

--- app/src/main/java/tw/idv/jew/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/tw/idv/jew/bitsandpizzas/OrderFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class OrderFragment : Fragment() {
 
@@ -19,6 +24,26 @@ class OrderFragment : Fragment() {
 
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                val text = "You need to choose a pizza type"
+                Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
+            } else {
+                var text = (when (pizzaType) {
+                    R.id.radio_diavolo -> "Diavolo pizza"
+                    else -> "Funghi pizza"
+                })
+                val parmesan = view.findViewById<Chip>(R.id.chip_parmesan)
+                text += if (parmesan.isChecked) ", extra parmesan" else ""
+                val chiliOil = view.findViewById<Chip>(R.id.chip_chili_oil)
+                text += if (chiliOil.isChecked) ", extra Chili oil" else ""
+                Snackbar.make(fab, text, Snackbar.LENGTH_LONG).show()
+            }
+        }
 
         return view
     }
